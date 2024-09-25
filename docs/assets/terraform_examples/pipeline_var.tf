17e277b5e7969e4d42a7a150ce4e35718c0ae258
Le <ule@meshcloud.io>
2024-09-25 15:56:30 +0200

    feat: correcting examples files link

---
+++ docs/assets/terraform_examples/pipeline_var.tf
@@ -0,0 +1,33 @@
+variable "github_token" {
+    description = "GitHub Personal Access Token"
+    type        = string
+    sensitive   = true
+  }
+  
+  variable "github_owner" {
+    description = "The owner of the GitHub repo containing the workflow."
+    type = string
+  }
+  
+  variable "github_repo" {
+    description = "The name of the GitHub repo containing the workflow."
+    type = string
+  }
+  
+  variable "workflow_create_file_name" {
+    default = "deploy.yml"
+    description = "The name of the workflow file (e.g. workflow.yml) for deploying the resources."
+    type = string
+  }
+  
+  variable "workflow_destroy_file_name" {
+    default = "destroy.yml"
+    description = "The name of the workflow file (e.g. workflow.yml) for destryoing the resources."
+    type = string
+  }
+  
+  variable "workflow_branch" {
+    default = "main"
+    description = "The name of the branch in which the workflow files live."
+    type = string
+  }
