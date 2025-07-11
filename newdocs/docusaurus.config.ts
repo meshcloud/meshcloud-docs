37e5bb4934498b5a66113abc5d5464a6d828cdab
Jelle den Burger <jdburger@meshcloud.io>
2025-07-11 10:25:36 +0200

    update colors

--- newdocs/docusaurus.config.ts
+++ newdocs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'meshStack Docs',
+  tagline: 'All-in-one platform management for your clouds',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -73,24 +73,23 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'meshStack',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'meshStack Logo',
+        src: 'img/meshstack_logo.png',
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
           position: 'right',
+          label: 'Docs',
         },
+        {
+          to: '/blog',
+          label: 'Release Notes',
+          position: 'right'
+        }
       ],
     },
     footer: {
