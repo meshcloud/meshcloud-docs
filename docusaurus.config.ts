d54aa7630748e884a8437e1960781d33577c9c14
Jelle den Burger <jdburger@meshcloud.io>
2025-07-16 17:33:12 +0200

    fix: root url not working

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,6 +47,20 @@ const config: Config = {
   },
   themes: ['@docusaurus/theme-mermaid'],
 
+  plugins: [
+      [
+        '@docusaurus/plugin-client-redirects',
+        {
+          redirects: [
+            {
+              from: '/',
+              to: '/docs'
+            },
+          ],
+        },
+      ]
+  ],
+
   presets: [
     [
       'classic',
@@ -57,7 +71,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/meshcloud/meshcloud-docs/tree/develop',
         },
         blog: {
           showReadingTime: true,
