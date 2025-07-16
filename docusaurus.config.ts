174651f4d5223dd2e3910bb394a4baa78dda2349
Jelle den Burger <jdburger@meshcloud.io>
2025-07-16 16:24:29 +0200

    fix: all docs should be behind docs/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
       'classic',
       {
         docs: {
-          routeBasePath: '/',
+          routeBasePath: '/docs',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
