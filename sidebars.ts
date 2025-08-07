179548785b20f7e262e17df5766e525c3b4d36ac
rebecca bausinger <r.bausinger@meshcloud.io>
2025-08-07 17:44:59 +0200

    migrated-operation-files

--- sidebars.ts
+++ sidebars.ts
@@ -216,7 +216,8 @@ const sidebars: SidebarsConfig = {
         'meshstack.monitoring',
         'meshstack.backup',
         'security-faq',
-        'copilot-preview',
+        'administration.dns', 
+        'administration.product-feedback-collection', // Product Feedback Collection
       ],
     },
     {
@@ -331,99 +332,115 @@ const sidebars: SidebarsConfig = {
             'new-guide-how-to-copilot'
           ],
         },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Integrations',
+      collapsible: true,
+      collapsed: true,
+      items: [
         {
           type: 'category',
-          label: 'Integrations',
-          collapsible: true,
-          collapsed: false,
+          label: 'Amazon Web Services',
           items: [
-            {
-              type: 'category',
-              label: 'Amazon Web Services',
-              items: [
-                'meshstack.aws.index',
-                'new-integration-how-to-integrate-aws',
-                'meshstack.aws.landing-zones',
-                'meshstack.aws.metering',
-                'meshstack.aws.sso-setup',
-                'meshstack.aws.reserved-instance-guide',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Microsoft Azure',
-              items: [
-                'meshstack.azure.index',
-                'new-integration-how-to-integrate-azure',
-                'new-integration-how-to-integrate-azure-administrative-units',
-                'meshstack.azure.landing-zones',
-                'meshstack.azure.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Google Cloud Platform',
-              items: [
-                'meshstack.gcp.index',
-                'new-guide-how-to-integrate-gcp',
-                'meshstack.gcp.landing-zones',
-                'meshstack.gcp.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Cloud Foundry',
-              items: [
-                'meshstack.cloudfoundry.index',
-                'meshstack.cloudfoundry.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Kubernetes',
-              items: [
-                'meshstack.kubernetes.index',
-                'meshstack.kubernetes.landing-zones',
-                'meshstack.kubernetes.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'GitHub',
-              items: [
-                'meshstack.github.pipeline-automation'
-              ],
-            },
-            {
-              type: 'category',
-              label: 'OpenShift',
-              items: [
-                'meshstack.openshift.index',
-                'meshstack.openshift.landing-zones',
-                'meshstack.openshift.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'OpenStack',
-              items: [
-                'meshstack.openstack.index',
-                'meshstack.openstack.metering',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'OSB Services',
-              items: [
-                'meshstack.meshmarketplace.index',
-                'meshstack.meshmarketplace.metering',
-                'meshstack.meshmarketplace.profile',
-                'meshstack.meshmarketplace.tenant-services',
-                'meshstack.meshmarketplace.broker-tutorial'
-              ],
-            },
+            'meshstack.aws.index',
+            'new-integration-how-to-integrate-aws',
+            'meshstack.aws.landing-zones',
+            'meshstack.aws.metering',
+            'meshstack.aws.sso-setup',
+            'meshstack.aws.reserved-instance-guide',
           ],
         },
+        {
+          type: 'category',
+          label: 'Microsoft Azure',
+          items: [
+            'meshstack.azure.index',
+            'new-integration-how-to-integrate-azure',
+            'new-integration-how-to-integrate-azure-administrative-units',
+            'meshstack.azure.landing-zones',
+            'meshstack.azure.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Google Cloud Platform',
+          items: [
+            'meshstack.gcp.index',
+            'new-guide-how-to-integrate-gcp',
+            'meshstack.gcp.landing-zones',
+            'meshstack.gcp.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Cloud Foundry',
+          items: [
+            'meshstack.cloudfoundry.index',
+            'meshstack.cloudfoundry.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Kubernetes',
+          items: [
+            'meshstack.kubernetes.index',
+            'meshstack.kubernetes.landing-zones',
+            'meshstack.kubernetes.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'GitHub',
+          items: [
+            'meshstack.github.pipeline-automation'
+          ],
+        },
+        {
+          type: 'category',
+          label: 'OpenShift',
+          items: [
+            'meshstack.openshift.index',
+            'meshstack.openshift.landing-zones',
+            'meshstack.openshift.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'OpenStack',
+          items: [
+            'meshstack.openstack.index',
+            'meshstack.openstack.metering',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'OSB Services',
+          items: [
+            'meshstack.meshmarketplace.index',
+            'meshstack.meshmarketplace.metering',
+            'meshstack.meshmarketplace.profile',
+            'meshstack.meshmarketplace.tenant-services',
+            'meshstack.meshmarketplace.broker-tutorial'
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Operations',
+      collapsible: true,
+      collapsed: true,
+      items: [
+        'meshstack.managed-service',
+        'meshstack.email',
+        'meshstack.logging',
+        'meshstack.monitoring',
+        'meshstack.backup',
+        'security-faq',
+        'administration.dns',
+        'administration.product-feedback-collection'
       ],
     },
   ],
