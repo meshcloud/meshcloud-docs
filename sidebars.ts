924149e5b4f4fac3dba853d9c9163b1ed3d736b3
Jelle den Burger <jdburger@meshcloud.io>
2025-08-21 14:22:40 +0200

    feat: Incorporate old tenant deletion page more in the new one

--- sidebars.ts
+++ sidebars.ts
@@ -261,7 +261,8 @@ const sidebars: SidebarsConfig = {
         'new-concept-communication',
         'new-concept-marketplace',
         'new-concept-osb-services',
-        'new-concept-copilot'
+        'new-concept-copilot',
+        'meshstack.identity-federation'
       ],
     },
     {
